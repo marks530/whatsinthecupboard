c3e1a0eb4b1f83c0f3f3ce8d0f020f642432fe62
Mark Smyth <marks530@gmail.com>
2021-12-02 19:52:28 +0000

    Initial commit

---
+++ .history/store/models_20211202103638.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
